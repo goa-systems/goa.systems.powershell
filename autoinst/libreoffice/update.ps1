aba85fe3b623c394c58e0e86631d7a54acf8c72d
Andreas Gottardi <ago@dom.schrotti12.eu>
2019-11-28 21:28:51 +0100

    Libreoffice added.

---
+++ autoinst/libreoffice/update.ps1
@@ -0,0 +1,9 @@
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+	Set-Location -Path "$PSScriptRoot"
+	.\uninstall.ps1
+	.\install.ps1
+} else {
+	$curscriptname = $MyInvocation.MyCommand.Name 
+	Start-Process -FilePath "powershell" -ArgumentList "$PSScriptRoot\$curscriptname" -Wait -Verb RunAs
+}
