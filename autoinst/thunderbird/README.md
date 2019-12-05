d690287b1a35662bcd832032d97fe36c6264b1dc
Andreas Gottardi <ago@dom.schrotti12.eu>
2019-12-05 20:23:52 +0100

    Mozilla Thunderbird added.

---
+++ autoinst/thunderbird/README.md
@@ -0,0 +1,2 @@
+# Mozilla Thunderbird installer
+Installs Mozilla Thunderbird systemwide.
---
+++ autoinst/thunderbird/install.ps1
@@ -0,0 +1,32 @@
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+	$name="thunderbird"
+	$version="68.3.0"
+	$setup="Thunderbird Setup $version.exe"
+	$dlurl="https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/$version/win64/en-US/Thunderbird%20Setup%20$version.exe"
+	
+	If (-Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\$name")) {
+		New-Item -Path "$env:SystemDrive\ProgramData\InstSys\$name" -ItemType "Directory"
+	}
+	
+	<# Download, if setup is not found in execution path. #>
+	if ( -Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\$name\$setup")) {
+		Start-BitsTransfer `
+		-Source "$dlurl"`
+		-Destination "$env:SystemDrive\ProgramData\InstSys\$name\$setup"
+	}
+	
+	Set-Content -Path "$env:SystemDrive\ProgramData\InstSys\$name\install.ini" -Value "[Install]"
+	Add-Content -Path "$env:SystemDrive\ProgramData\InstSys\$name\install.ini" -Value "InstallDirectoryPath=C:\Program Files\Mozilla Thunderbird"
+	Add-Content -Path "$env:SystemDrive\ProgramData\InstSys\$name\install.ini" -Value "QuickLaunchShortcut=false"
+	Add-Content -Path "$env:SystemDrive\ProgramData\InstSys\$name\install.ini" -Value "TaskbarShortcut=false"
+	Add-Content -Path "$env:SystemDrive\ProgramData\InstSys\$name\install.ini" -Value "DesktopShortcut=false"
+	Add-Content -Path "$env:SystemDrive\ProgramData\InstSys\$name\install.ini" -Value "StartMenuShortcuts=true"
+	Add-Content -Path "$env:SystemDrive\ProgramData\InstSys\$name\install.ini" -Value "StartMenuDirectoryName=Mozilla Thunderbird"
+	Add-Content -Path "$env:SystemDrive\ProgramData\InstSys\$name\install.ini" -Value "MaintenanceService=false"
+	
+	Start-Process -FilePath "$env:SystemDrive\ProgramData\InstSys\$name\$setup" -ArgumentList "/INI=$env:SystemDrive\ProgramData\InstSys\$name\install.ini" -Wait
+} else {
+	$curscriptname = $MyInvocation.MyCommand.Name 
+	Start-Process -FilePath "powershell" -ArgumentList "$PSScriptRoot\$curscriptname" -Wait -Verb RunAs
+}
