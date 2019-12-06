da150c0a754c3fdd4923e9040eeade7810650398
Andreas Gottardi <ago@dom.schrotti12.eu>
2019-12-06 06:37:20 +0100

    Notepad++ updated to 7.8.2.

--- autoinst/notepad++/install.ps1
+++ autoinst/notepad++/install.ps1
@@ -1,6 +1,6 @@
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 if($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-	$nppvers = "7.8.1"
+	$nppvers = "7.8.2"
 	$nppsetup = "npp.$nppvers.Installer.x64.exe"
 	$dlurl = "http://download.notepad-plus-plus.org/repository/7.x/$nppvers/$nppsetup"
 	If(-Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\npp")){
