26a2021ec2ea7b9a3086f884a8d6d042994e0709
Andreas Gottardi <ago@dom.schrotti12.eu>
2019-12-09 18:07:33 +0100

    Cleanup.
    Unnecessary .gitignore removed.
    update.ps1 files normalized.

--- autoinst/winmerge/update.ps1
+++ autoinst/winmerge/update.ps1
@@ -1,8 +1,8 @@
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 if($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
 	Set-Location -Path "$PSScriptRoot"
-	.\uninstall.ps1
-	.\install.ps1
+	Start-Process -Wait -FilePath "powershell.exe" -ArgumentList "-File","uninstall.ps1"
+	Start-Process -Wait -FilePath "powershell.exe" -ArgumentList "-File","install.ps1"
 } else {
 	$curscriptname = $MyInvocation.MyCommand.Name 
 	Start-Process -FilePath "powershell" -ArgumentList "$PSScriptRoot\$curscriptname" -Wait -Verb RunAs
