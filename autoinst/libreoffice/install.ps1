7ab182b39be69b239b52a66deab366326428ef09
Andreas Gottardi <ago@dom.schrotti12.eu>
2019-12-12 19:42:10 +0100

    Libreoffice updated to 6.3.4.

--- autoinst/libreoffice/install.ps1
+++ autoinst/libreoffice/install.ps1
@@ -1,7 +1,7 @@
 $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
 	$name = "libreoffice"
-	$version = "6.3.3"
+	$version = "6.3.4"
 	$setup = "LibreOffice_${version}_Win_x64.msi"
 	$dlurl = "https://ftp.gwdg.de/pub/tdf/libreoffice/stable/$version/win/x86_64/$setup"
 
