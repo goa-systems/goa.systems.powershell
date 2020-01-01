3d42204fb974b54c9f7ba486f7299b082980f530
Andreas Gottardi <ago@dom.schrotti12.eu>
2020-01-01 17:42:38 +0100

    KeePass installer added.

---
+++ autoinst/keepass/uninstall.ps1
@@ -0,0 +1,17 @@
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+	if(Test-Path -Path "$env:ProgramFiles\KeePass") {
+		Remove-Item -Path "$env:ProgramFiles\KeePass" -Recurse
+	}
+	
+	if(Test-Path -Path "$env:Public\Desktop\KeePass.lnk") {
+		Remove-Item -Path "$env:Public\Desktop\KeePass.lnk"
+	}
+	
+	if(Test-Path -Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\KeePass.lnk") {
+		Remove-Item -Path "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\KeePass.lnk"
+	}
+} else {
+	$curscriptname = $MyInvocation.MyCommand.Name 
+	Start-Process -FilePath "powershell" -ArgumentList "$PSScriptRoot\$curscriptname" -Wait -Verb RunAs
+}
