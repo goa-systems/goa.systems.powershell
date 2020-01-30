61f3bfb2261030556c4bf814671dc35e18d37d04
Andreas Gottardi <ago@dom.schrotti12.eu>
2020-01-30 21:20:32 +0100

    Chromium: check for presence of 7z.exe added.

--- autoinst/chromium_user/install.ps1
+++ autoinst/chromium_user/install.ps1
@@ -16,18 +16,24 @@ if( -Not (Test-Path -Path "$OutPath")){
 	Invoke-WebRequest -Uri "$Url" -OutFile "$OutPath"
 }
 
-if(-Not (Test-Path -Path "$DestinationPath")){
-	New-Item -ItemType "Directory" -Path "$DestinationPath"
-}
-Start-Process -FilePath "7z.exe" -ArgumentList "x","-aos","`"-o$DestinationPath`"","-bb0","-bse0","-bsp2","-pdefault","-sccUTF-8","`"$OutPath`"" -Wait -NoNewWindow
-if(-Not (Test-Path -Path "$env:AppData\Microsoft\Windows\Start Menu\Programs\Chromium.lnk")){
-	..\insttools\CreateShortcut.ps1 `
-		-LinkName "Chromium" `
-		-TargetPath "$DestinationPath\$FolderName\chrome.exe" `
-		-Arguments "" `
-		-IconFile "$DestinationPath\$FolderName\chrome.exe" `
-		-IconId 0 `
-		-Description "Chromium Browser" `
-		-WorkingDirectory "%UserProfile%" `
-		-ShortcutLocations @("$env:AppData\Microsoft\Windows\Start Menu\Programs")
+try { 7z | Out-Null } catch {}
+if(-not ($?)){
+	Write-Host -Object "Can not find 7z.exe. Please check your global path and rerun installer."
+	Start-Sleep -s 10
+} else {
+	if(-Not (Test-Path -Path "$DestinationPath")){
+		New-Item -ItemType "Directory" -Path "$DestinationPath"
+	}
+	Start-Process -FilePath "7z.exe" -ArgumentList "x","-aos","`"-o$DestinationPath`"","-bb0","-bse0","-bsp2","-pdefault","-sccUTF-8","`"$OutPath`"" -Wait -NoNewWindow
+	if(-Not (Test-Path -Path "$env:AppData\Microsoft\Windows\Start Menu\Programs\Chromium.lnk")){
+		..\insttools\CreateShortcut.ps1 `
+			-LinkName "Chromium" `
+			-TargetPath "$DestinationPath\$FolderName\chrome.exe" `
+			-Arguments "" `
+			-IconFile "$DestinationPath\$FolderName\chrome.exe" `
+			-IconId 0 `
+			-Description "Chromium Browser" `
+			-WorkingDirectory "%UserProfile%" `
+			-ShortcutLocations @("$env:AppData\Microsoft\Windows\Start Menu\Programs")
+	}
 }
