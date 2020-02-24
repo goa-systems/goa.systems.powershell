072a5238577cbea22cb8d8a4ad70f0eedab861a1
Andreas Gottardi <ago@dom.schrotti12.eu>
2020-02-24 19:27:51 +0100

    Gimp updated.

--- autoinst/gimp/install.ps1
+++ autoinst/gimp/install.ps1
@@ -1,5 +1,5 @@
 if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-	$gimpssetup="gimp-2.10.14-setup-3.exe"
+	$gimpssetup="gimp-2.10.18-setup.exe"
 
 	If(-Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\gimp")){
 		New-Item -Path "$env:SystemDrive\ProgramData\InstSys\gimp" -ItemType "Directory"
