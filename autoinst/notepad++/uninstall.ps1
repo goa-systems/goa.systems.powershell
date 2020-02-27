ff5e80f9c26e086ceb5f911f11377c03b3518788
Andreas Gottardi <ago@dom.schrotti12.eu>
2020-02-27 21:19:19 +0100

    Explorer shutdown added to uninstall script.

--- autoinst/notepad++/uninstall.ps1
+++ autoinst/notepad++/uninstall.ps1
@@ -1,5 +1,5 @@
 if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-	$processes = @("notepad++", "explorer")
+	$processes = @("notepad++")
 	foreach($process in $processes){
 		$p = Get-Process "$process" -ErrorAction SilentlyContinue
 		if ($p) {
@@ -36,8 +36,18 @@ if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsI
 			Write-Host -Object "Uninststring not found."
 		} else {
 			Write-Host -Object "Uninststring found $uninststring"
+			$AutoRestartShellEnabled = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "AutoRestartShell" | Select-Object -ExpandProperty "AutoRestartShell")
+			if($AutoRestartShellEnabled -eq 1){
+				# Temporary disable automatic restarting of during uninstall.
+				Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "AutoRestartShell" -Value 0
+			}
+			Get-Process "explorer" -ErrorAction SilentlyContinue | Stop-Process -Force
 			Start-Process -Wait -FilePath "$uninststring" -ArgumentList "/S"
-			Start-Process -FilePath "explorer.exe" -ArgumentList "/S"
+			Start-Process -FilePath "explorer"
+			if($AutoRestartShellEnabled -eq 1){
+				# Restore the original setting.
+				Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "AutoRestartShell" -Value 1
+			}
 		}
 	}
 } else {
