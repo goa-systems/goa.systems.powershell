3d19cf63faa40ff35a508a07ba58c7df63322d07
Andreas Gottardi <ago@dom.schrotti12.eu>
2020-03-12 19:07:56 +0100

    Download of additional plugins is now validated.

--- autoinst/eclipse/eclipse.ps1
+++ autoinst/eclipse/eclipse.ps1
@@ -45,57 +45,66 @@ Move-Item "$WorkingDirectory\Eclipse.tmp\eclipse" "$WorkingDirectory\Eclipse"
 Remove-Item "$WorkingDirectory\Eclipse.zip"
 Remove-Item "$WorkingDirectory\Eclipse.tmp" -Recurse
 
+$PluginInstallSuccess = $true
+
 foreach($Plugin in $AdditionalPlugins){
 	try {
 		# Invoke-WebRequest -Uri "$Plugin" -OutFile "$WorkingDirectory\Eclipse\plugins"
 		Start-BitsTransfer -Source "$Plugin" -Destination "$WorkingDirectory\Eclipse\plugins" -ErrorAction Stop
 	} catch {
 		Write-Error -Message "There has been a download error with the url ${Plugin}."
+		<# If one error is thrown, the plugins are not installed correctly. #>
+		$PluginInstallSuccess = $PluginInstallSuccess -and $false
 	}
 }
 
-$ReposString = Convert-ArrayToString -StringArray $Repos
+if($PluginInstallSuccess){
 
-# Start and wait for user to finish modifications
-# New-Item -ItemType Directory -Path "$WorkingDirectory\WorkSpace"
-$i = 1
-$Plc = $FeatureList.Count
+	$ReposString = Convert-ArrayToString -StringArray $Repos
 
-foreach($Feature in $FeatureList){
-	
-	Write-Host -Object "Installing plugin ${i} of ${Plc}: ${Feature}"
+	# Start and wait for user to finish modifications
+	# New-Item -ItemType Directory -Path "$WorkingDirectory\WorkSpace"
+	$i = 1
+	$Plc = $FeatureList.Count
 
-	Start-Process `
-		-FilePath "$WorkingDirectory\Eclipse\eclipse.exe" `
-		-Wait `
-		-ArgumentList `
-			"-application","org.eclipse.equinox.p2.director",`
-			"-repository","$ReposString",`
-			"-installIU","$Feature"
-	$i++;
+	foreach($Feature in $FeatureList){
+		
+		Write-Host -Object "Installing plugin ${i} of ${Plc}: ${Feature}"
+
+		Start-Process `
+			-FilePath "$WorkingDirectory\Eclipse\eclipse.exe" `
+			-Wait `
+			-ArgumentList `
+				"-application","org.eclipse.equinox.p2.director",`
+				"-repository","$ReposString",`
+				"-installIU","$Feature"
+		$i++;
+	}
+
+
+	# 
+	# Configure Workspace
+	$SettingsPath = "$WorkingDirectory\WorkSpace\.metadata\.plugins\org.eclipse.core.runtime\.settings"
+
+	If (-Not (Test-Path -Path "$SettingsPath")) {
+		New-Item -ItemType Directory -Path "$SettingsPath"
+	}
+
+	$configfiles = @(
+		"org.eclipse.core.resources.prefs",
+		"org.eclipse.core.runtime.prefs",
+		"org.eclipse.e4.ui.workbench.renderers.swt.prefs",
+		"org.eclipse.ui.editors.prefs",
+		"org.eclipse.ui.ide.prefs",
+		"org.eclipse.ui.prefs"
+	)
+
+	foreach($file in $configfiles){
+		Copy-Item -Path "conf\$file" -Destination "$SettingsPath\$file"
+	}
+
+	# Start eclipse for further customization
+	Start-Process -FilePath "$WorkingDirectory\Eclipse\eclipse.exe" -ArgumentList "-data","$WorkingDirectory\WorkSpace" -Wait
+} else {
+	Write-Host -Object "Additional plugins were net installed successfully. Eclipse not configured."
 }
-
-
-# 
-# Configure Workspace
-$SettingsPath = "$WorkingDirectory\WorkSpace\.metadata\.plugins\org.eclipse.core.runtime\.settings"
-
-If (-Not (Test-Path -Path "$SettingsPath")) {
-	New-Item -ItemType Directory -Path "$SettingsPath"
-}
-
-$configfiles = @(
-	"org.eclipse.core.resources.prefs",
-	"org.eclipse.core.runtime.prefs",
-	"org.eclipse.e4.ui.workbench.renderers.swt.prefs",
-	"org.eclipse.ui.editors.prefs",
-	"org.eclipse.ui.ide.prefs",
-	"org.eclipse.ui.prefs"
-)
-
-foreach($file in $configfiles){
-	Copy-Item -Path "conf\$file" -Destination "$SettingsPath\$file"
-}
-
-# Start eclipse for further customization
-Start-Process -FilePath "$WorkingDirectory\Eclipse\eclipse.exe" -ArgumentList "-data","$WorkingDirectory\WorkSpace" -Wait
