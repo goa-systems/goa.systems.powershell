2ec73728cc58e3989a23d4e007e16f3765b942ee
Andreas Gottardi <ago@dom.schrotti12.eu>
2020-03-17 20:46:55 +0100

    Unattended update installer added.

---
+++ tools/UpdateInstaller/InstallUpdates.ps1
@@ -0,0 +1,19 @@
+param (
+	[Parameter(Mandatory=$true)]
+	[String]
+	$Directory
+)
+
+if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+	if(Test-Path -Path "$Directory"){
+		Write-Host -Object "Starting installation. This computer will restart as soon as all updates are installed."
+		Get-ChildItem -Path "$Directory" | Select-Object -Property "Name" | Where-Object {$_.Name -like 'Windows*'} | ForEach-Object {
+			Start-Process -Wait -FilePath "wusa.exe" -ArgumentList "`"$Directory\$($_.Name)`"","/quiet","/norestart"
+		}
+		Restart-Computer
+	} else {
+		Write-Host -Object "Directory not found."
+	}
+} else {
+	Start-Process -FilePath "powershell" -ArgumentList "$PSScriptRoot\$($MyInvocation.MyCommand.Name)","-Directory","`"$Directory`"" -Verb RunAs
+}
---
+++ tools/UpdateInstaller/README.md
@@ -0,0 +1,5 @@
+# Unattended update installer
+This script installs all msu files from a given directory and restarts the computer as soon as all updates are installed.
+```powershell
+.\InstallUpdates.ps1 -Directory "C:\Path\to\update\directory"
+```
