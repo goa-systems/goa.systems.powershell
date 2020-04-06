bf48096c7489678b5a4e7150b42c0d197347c87f
Andreas Gottardi <ago@dom.schrotti12.eu>
2020-04-06 21:11:52 +0200

    ArrayTokenizer optimized.
    Delimiter option added.
    Quote string option added.

--- tools/ArrayTokenizer/ArrayTokenizer.ps1
+++ tools/ArrayTokenizer/ArrayTokenizer.ps1
@@ -1,12 +1,19 @@
 param (
 	# Array of strings
 	[String[]]
-	$StringArray = @("These","are","the","default","elements.")
+	$StringArray = @("These","are","the","default","elements."),
+
+	# The default delimiter
+	[String]
+	$Delimiter = ",",
+
+	# The default delimiter
+	[String]
+	$QuoteChar = ""
 )
 $String = ""
-for ($i = 0; $i -lt $StringArray.Count - 1; $i++) {
-	$Element = $StringArray[$i]
-	$String += "$Element,"
+for ($i = 0; $i -lt $StringArray.Count - 1; $i++) { 
+	$String += "${QuoteChar}$($StringArray[$i])${QuoteChar}$Delimiter"
 }
-$String += $StringArray[$i]
+$String += "${QuoteChar}$($StringArray[$i])${QuoteChar}"
 return $String
