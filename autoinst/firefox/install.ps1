c71d28fb2ed07e858775749f9243c90cf6e5e57d
Andreas Gottardi <ago@dom.schrotti12.eu>
2020-05-08 23:09:05 +0200

    Firefox updated.

--- autoinst/firefox/install.ps1
+++ autoinst/firefox/install.ps1
@@ -1,5 +1,5 @@
 if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-	$ffvers = "76.0"
+	$ffvers = "76.0.1"
 	$ffsetup="Firefox Setup $ffvers.exe"
 
 	$lang="en-US"
