29bdd2b48c933c00cef3ac899153dfb637e53fb7
Andreas Gottardi <ago@dom.schrotti12.eu>
2020-06-11 10:23:30 +0200

    Uninstaller added.

---
+++ autoinst/apache/uninstall.ps1
@@ -0,0 +1,14 @@
+param (
+	# The certificate file.
+	[String] $ApacheVersion = "2.4.43"
+)
+
+if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    
+    Stop-Service -DisplayName "Apache web server" -Force
+    Start-Process -FilePath "$env:ProgramFiles\Apache\$ApacheVersion\bin\httpd.exe" -ArgumentList @("-k","uninstall","-n","`"Apache web server`"") -Wait
+    Remove-Item -Path "$env:ProgramFiles\Apache\$ApacheVersion" -Recurse
+
+} else {
+	Start-Process -FilePath "powershell" -ArgumentList "$PSScriptRoot\$($MyInvocation.MyCommand.Name)","-ApacheVersion","`"$ApacheVersion`"" -Wait -Verb RunAs
+}
