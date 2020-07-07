e5aee601bff764b9c6ab7c0a007e7dbfb7626dde
andreasgottardi <andreas.gottardi@gmx.at>
2020-07-07 19:13:11 +0200

    PHP updated.

--- autoinst/php/install.ps1
+++ autoinst/php/install.ps1
@@ -1,6 +1,6 @@
 param (
 	# The certificate file.
-	[String] $PhpVersion = "7.4.7"
+	[String] $PhpVersion = "7.4.8"
 )
 
 if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
