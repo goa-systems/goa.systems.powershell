1fec3e58f6febe568e01885648d263b56c91ec41
Andreas Gottardi <ago@dom.schrotti12.eu>
2020-08-27 12:10:31 +0200

    Windows terminal installer added.

---
+++ autoinst/winterm_user/install.ps1
@@ -0,0 +1,22 @@
+$CurrentVersion = "1.2.2381.0"
+$FileName = "Microsoft.WindowsTerminal_${CurrentVersion}_8wekyb3d8bbwe.msixbundle"
+$DownloadUrl = "https://github.com/microsoft/terminal/releases/download/v${CurrentVersion}/$FileName"
+$ConfigFile = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+
+$DownloadDir = "$env:ProgramData\InstSys\wt"
+
+if(-not (Test-Path -Path "$DownloadDir")){
+	New-Item -ItemType "Directory" -Path "$DownloadDir"
+}
+
+$ProgressPreference = 'SilentlyContinue'
+if(-not (Test-Path -Path "$DownloadDir\$FileName")){
+    Invoke-WebRequest -Uri "$DownloadUrl" -OutFile "$DownloadDir\$FileName"
+}
+
+Add-AppxPackage -Path "$DownloadDir\$FileName"
+
+if(Test-Path -Path "$env:TEMP\settings.wt.backup.json"){
+    Write-Host -Object "Restoring configuration from backup."
+    Copy-Item -Path "$env:TEMP\settings.wt.backup.json" -Destination $ConfigFile
+}
