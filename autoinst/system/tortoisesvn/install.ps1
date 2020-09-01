45b6b256bc9fab0d3ecce9ff633b86147f698ef4
Andreas Gottardi <ago@dom.schrotti12.eu>
2020-09-01 21:08:26 +0200

    TortoiseSVN updated.

--- autoinst/system/tortoisesvn/install.ps1
+++ autoinst/system/tortoisesvn/install.ps1
@@ -1,19 +1,23 @@
 if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
 
-	$version = "1.13.1"
-	$setup = "TortoiseSVN-$version.28686-x64-svn-1.13.0.msi"
+	$Version = "1.14.0"
+	$Revision = "28885"
+	$Setup = "TortoiseSVN-$Version.$Revision-x64-svn-$Version.msi"
 
 	If(-Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\tsvn")){
 		New-Item -Path "$env:SystemDrive\ProgramData\InstSys\tsvn" -ItemType "Directory"
 	}
 
 	<# Download tsvn scm, if setup is not found in execution path. #>
-	if( -Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\tsvn\$setup")){
+	if( -Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\tsvn\$Setup")){
+		Write-Host -Object "Downloading TortoiseSVN"
+		$ProgressPreference = 'SilentlyContinue'
 		Invoke-WebRequest `
-		-Uri "https://dotsrc.dl.osdn.net/osdn/storage/g/t/to/tortoisesvn/$version/Application/$setup" `
-		-OutFile "$env:SystemDrive\ProgramData\InstSys\tsvn\$setup"
+		-Uri "https://dotsrc.dl.osdn.net/osdn/storage/g/t/to/tortoisesvn/$Version/Application/$Setup" `
+		-OutFile "$env:SystemDrive\ProgramData\InstSys\tsvn\$Setup"
+		Write-Host -Object "Download done"
 	}
-	Start-Process -Wait -FilePath "msiexec" -ArgumentList "/qb","/i","$env:SystemDrive\ProgramData\InstSys\tsvn\$setup","INSTALLDIR=`"C:\Program Files\TortoiseSVN`"","ADDLOCAL=ALL"
+	Start-Process -Wait -FilePath "msiexec" -ArgumentList "/qb","/i","$env:SystemDrive\ProgramData\InstSys\tsvn\$Setup","INSTALLDIR=`"C:\Program Files\TortoiseSVN`"","ADDLOCAL=ALL"
 } else {
 	Start-Process -FilePath "powershell" -ArgumentList "$PSScriptRoot\$($MyInvocation.MyCommand.Name)" -Wait -Verb RunAs
 }
