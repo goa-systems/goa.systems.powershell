0e4e51982695620235cac0684d1a8e8224ee3190
andreasgottardi <andreas.gottardi@gmx.at>
2020-09-10 21:56:44 +0200

    Postman modified.
    Desktop shortcut removed after install.

--- autoinst/user/postman/install.ps1
+++ autoinst/user/postman/install.ps1
@@ -8,3 +8,8 @@ New-Item -ItemType "Directory" -Path "$DownloadDir"
 $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri "$Url" -OutFile "$DownloadDir\setup.exe"
 Start-Process -FilePath "$DownloadDir\setup.exe" -ArgumentList "-s" -Wait
+
+$ShortCut = "$env:USERPROFILE\Desktop\Postman.lnk"
+if(Test-Path -Path "$ShortCut"){
+	Remove-Item -Path "$ShortCut" -Force
+}
