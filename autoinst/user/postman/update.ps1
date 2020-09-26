9adf509ff13f28cc11c72218ce645943a3ca3b4b
andreasgottardi <andreas.gottardi@gmx.at>
2020-09-26 23:43:51 +0200

    UAC removed from Postman installer.

--- autoinst/user/postman/update.ps1
+++ autoinst/user/postman/update.ps1
@@ -1,6 +1,2 @@
-if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-	Set-Location -Path "$PSScriptRoot"
-	Start-Process -FilePath "powershell.exe" -ArgumentList ".\uninstall.ps1;.\install.ps1"
-} else {
-	Start-Process -FilePath "powershell" -ArgumentList "$PSScriptRoot\$($MyInvocation.MyCommand.Name)" -Verb RunAs
-}
+Set-Location -Path "$PSScriptRoot"
+Start-Process -FilePath "powershell.exe" -ArgumentList ".\uninstall.ps1;.\install.ps1"
