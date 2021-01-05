34ef7847dc0440c15457a6ba8b9cffb24e4199c0
AndreasGottardi <andreas.gottardi@gmx.at>
2021-01-05 10:51:35 +0100

    Filezilla download simplified.

--- autoinst/system/filezilla/install.ps1
+++ autoinst/system/filezilla/install.ps1
@@ -4,24 +4,21 @@ if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsI
 	Set-Location -Path "$PSScriptRoot"
 	$Json = Get-Content -Raw -Path "version.json" | ConvertFrom-Json
 
-	$fzsetup="FileZilla_$($Json.version)_win64-setup.exe"
+	$FilezillaSetup="FileZilla_$($Json.version)_win64-setup.exe"
+	$DownloadUrl = "https://download.filezilla-project.org/client/$FilezillaSetup"
 
 	If(-Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\filezilla")){
 		New-Item -Path "$env:SystemDrive\ProgramData\InstSys\filezilla" -ItemType "Directory"
 	}
 	
 	<# Download Filezilla, if setup is not found in execution path. #>
-	if( -Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\filezilla\$fzsetup")){
-		$pattern= "<a href=`"(.*)`" rel=`"nofollow`">$fzsetup</a>"
-		Invoke-WebRequest -Uri "https://filezilla-project.org/download.php?show_all=1" -OutFile "$env:TEMP\fzdl.html"
-		$dllink = (Select-String -Path "$env:TEMP\fzdl.html" -Pattern $pattern).Matches.Groups[1].Value.Trim()
+	if( -Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\filezilla\$FilezillaSetup")){
 		Write-Host -Object "Downloading FileZilla."
 		$ProgressPreference = 'SilentlyContinue'
-		Invoke-WebRequest -Uri $dllink -OutFile "$env:SystemDrive\ProgramData\InstSys\filezilla\$fzsetup"
-		Remove-Item -Path "$env:TEMP\fzdl.html"
+		Invoke-WebRequest -Uri $DownloadUrl -OutFile "$env:SystemDrive\ProgramData\InstSys\filezilla\$FilezillaSetup"
 	}
 	Start-Process -Wait `
-		-FilePath "$env:SystemDrive\ProgramData\InstSys\filezilla\$fzsetup" `
+		-FilePath "$env:SystemDrive\ProgramData\InstSys\filezilla\$FilezillaSetup" `
 		-ArgumentList "/S","/D=`"$env:PROGRAMFILES\FileZilla`""
 
 } else {
