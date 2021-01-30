37e68b105e91d7a2a55e6c5a149bd3bf633173ce
AndreasGottardi <andreas.gottardi@gmx.at>
2021-01-30 14:30:56 +0100

    Error in version.json fixed.

--- autoinst/system/winmerge/install.ps1
+++ autoinst/system/winmerge/install.ps1
@@ -6,14 +6,14 @@ if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsI
 	$apptitle="WinMerge"
 	$version = $Json.version
 	$setup="winmerge-$version-x64-exe.zip"
-	$dlurl="https://netcologne.dl.sourceforge.net/project/winmerge/stable/$version/$setup"
+	$DownloadUrl="https://netcologne.dl.sourceforge.net/project/winmerge/stable/$version/$setup"
 	If (-Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\$name")) {
 		New-Item -Path "$env:SystemDrive\ProgramData\InstSys\$name" -ItemType "Directory"
 	}
 	
 	<# Download, if setup is not found in execution path. #>
 	if ( -Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\$name\$setup")) {
-		Start-BitsTransfer -Source "$dlurl" -Destination "$env:SystemDrive\ProgramData\InstSys\$name\$setup"
+		Start-BitsTransfer -Source "$DownloadUrl" -Destination "$env:SystemDrive\ProgramData\InstSys\$name\$setup"
 	}
 	
 	Expand-Archive -Path "$env:SystemDrive\ProgramData\InstSys\$name\$setup" -DestinationPath "$env:ProgramFiles"
