edb1d2f4c6b4f2f92be11b8f693930fe0fa0263f
Andreas Gottardi <andreas.gottardi@gmx.at>
2021-05-05 22:22:04 +0200

    Installer fixed.

--- autoinst/system/winmerge/install.ps1
+++ autoinst/system/winmerge/install.ps1
@@ -6,7 +6,7 @@ if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsI
 	$apptitle="WinMerge"
 	$version = $Json.version
 	$setup="winmerge-$($version.Substring(1))-x64-exe.zip"
-	$DownloadUrl="https://github.com/WinMerge/winmerge/releases/download/v2.16.10/$setup"
+	$DownloadUrl="https://github.com/WinMerge/winmerge/releases/download/$version/$setup"
 	If (-Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\$name")) {
 		New-Item -Path "$env:SystemDrive\ProgramData\InstSys\$name" -ItemType "Directory"
 	}
