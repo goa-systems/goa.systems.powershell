6e085fa352b74c3287569cae3db28bfeb8a79d3b
andreasgottardi <andreas.gottardi@gmx.at>
2021-05-08 14:02:04 +0200

    Putty updated.
    Changed to Json based versioning.

--- autoinst/system/putty/install.ps1
+++ autoinst/system/putty/install.ps1
@@ -1,19 +1,23 @@
 if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-	$vers = "0.74"
-	$setup = "putty-64bit-$vers-installer.msi"
+	
+	$Json = Get-Content -Raw -Path "version.json" | ConvertFrom-Json
+	$Version = $Json.version
+	
+	$SetupFile = "putty-64bit-$Version-installer.msi"
+	$DownloadUrl = "https://the.earth.li/~sgtatham/putty/$Version/w64/$SetupFile"
 	
 	If(-Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\putty")){
 		New-Item -Path "$env:SystemDrive\ProgramData\InstSys\putty" -ItemType "Directory"
 	}
 	
 	<# Download putty, if setup is not found in execution path. #>
-	if( -Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\putty\$setup")){
+	if( -Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\putty\$SetupFile")){
 		Start-BitsTransfer `
-		-Source "https://the.earth.li/~sgtatham/putty/$vers/w64/$setup" `
-		-Destination "$env:SystemDrive\ProgramData\InstSys\putty\$setup"
+		-Source "$DownloadUrl" `
+		-Destination "$env:SystemDrive\ProgramData\InstSys\putty\$SetupFile"
 	}
 	
-	Start-Process -Wait -FilePath "msiexec" -ArgumentList "/qb","/i","$env:SystemDrive\ProgramData\InstSys\putty\$setup","/passive","INSTALLDIR=`"C:\Program Files\Putty`"","ADDLOCAL=FilesFeature,PathFeature,PPKFeature"	
+	Start-Process -Wait -FilePath "msiexec" -ArgumentList "/qb","/i","$env:SystemDrive\ProgramData\InstSys\putty\$SetupFile","/passive","INSTALLDIR=`"C:\Program Files\Putty`"","ADDLOCAL=FilesFeature,PathFeature,PPKFeature"	
 } else {
 	Start-Process -FilePath "powershell" -ArgumentList "$PSScriptRoot\$($MyInvocation.MyCommand.Name)" -Wait -Verb RunAs
 }
