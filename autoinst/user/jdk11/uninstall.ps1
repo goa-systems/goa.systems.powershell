0ac88ac6259e0d72be864d9e4e001392391d1f43
andreasgottardi <andreas.gottardi@gmx.at>
2022-01-21 22:57:19 +0100

    Adding JDK11 installer.

---
+++ autoinst/user/jdk11/uninstall.ps1
@@ -0,0 +1,15 @@
+$processes = @("java", "javaw", "eclipse", "netbeans")
+foreach($process in $processes){
+	Get-Process -Name "$process" -ErrorAction SilentlyContinue | Stop-Process -ErrorAction SilentlyContinue
+}
+
+# Delete directories
+$TargetPath = "$env:LOCALAPPDATA\Programs\Java"
+
+Get-ChildItem -Path "$TargetPath" | Where-Object {$_.Name -match "(.*)jdk8(.*)"} | ForEach-Object {
+	Remove-Item -Recurse -Force -Path "$($_.FullName)"
+}
+
+if("$env:JAVA_HOME_11" -ne ""){
+	[System.Environment]::SetEnvironmentVariable('JAVA_HOME_11', $null, [System.EnvironmentVariableTarget]::User)
+}
