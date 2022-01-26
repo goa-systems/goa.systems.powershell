2a00a3f1bc36136b3df216da6f72f25d45c60b6e
AndreasGottardi <andreas.gottardi@gmx.at>
2022-01-26 21:03:25 +0100

    Fixing relative path definitions.

--- autoinst/user/java/install.ps1
+++ autoinst/user/java/install.ps1
@@ -1,5 +1,5 @@
-& .\..\jdk8\install.ps1
-& .\..\jdk11\install.ps1
-& .\..\jdk17\install.ps1
+& $PSScriptRoot\..\jdk8\install.ps1
+& $PSScriptRoot\..\jdk11\install.ps1
+& $PSScriptRoot\..\jdk17\install.ps1
 
 [System.Environment]::SetEnvironmentVariable('JAVA_HOME',"$env:JAVA_HOME_17", [System.EnvironmentVariableTarget]::User)
--- autoinst/user/java/uninstall.ps1
+++ autoinst/user/java/uninstall.ps1
@@ -1,5 +1,5 @@
-& .\..\jdk8\uninstall.ps1
-& .\..\jdk11\uninstall.ps1
-& .\..\jdk17\uninstall.ps1
+& $PSScriptRoot\..\jdk8\uninstall.ps1
+& $PSScriptRoot\..\jdk11\uninstall.ps1
+& $PSScriptRoot\..\jdk17\uninstall.ps1
 
 [System.Environment]::SetEnvironmentVariable('JAVA_HOME',$null, [System.EnvironmentVariableTarget]::User)
