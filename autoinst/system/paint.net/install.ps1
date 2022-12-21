5ad11a7b7bbdafb39ff9249030027cec506e161b
AndreasGottardi <andreas.gottardi@gmx.at>
2022-12-21 21:29:38 +0100

    Fixing Paint.NET installer.

--- autoinst/system/paint.net/install.ps1
+++ autoinst/system/paint.net/install.ps1
@@ -4,37 +4,29 @@ if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsI
 	$Json = Get-Content -Raw -Path "version.json" | ConvertFrom-Json
 	$name = "paintnet"
 	$version = $Json.version
-	$setup = "paint.net.$version.install.zip"
-	$dlurl = "https://www.dotpdn.com/files/$setup"
+	$setup = "paint.net.${version}.winmsi.x64.zip"
+	$dlurl = "https://github.com/paintdotnet/release/releases/download/v${version}/$setup"
 
 
-	If(-Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\$name")){
-	New-Item -Path "$env:SystemDrive\ProgramData\InstSys\$name" -ItemType "Directory"
+	If(Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\$name"){
+		Remove-Item -Path "$env:SystemDrive\ProgramData\InstSys\$name" -Recurse -Force
 	}
+	New-Item -Path "$env:SystemDrive\ProgramData\InstSys\$name" -ItemType "Directory"
+	New-Item -Path "$env:SystemDrive\ProgramData\InstSys\$name\extract" -ItemType "Directory"
 
 	<# Download if setup is not found in execution path. #>
 	if( -Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\$name\$setup")){
-	Write-Host "Downloading from $dlurl"
-	Start-BitsTransfer `
-	-Source $dlurl `
-	-Destination "$env:SystemDrive\ProgramData\InstSys\$name\$setup"
+		Write-Host "Downloading from $dlurl"
+		Start-BitsTransfer `
+		-Source $dlurl `
+		-Destination "$env:SystemDrive\ProgramData\InstSys\$name\$setup"
 	}
 
-	if(Test-Path -Path "$env:TEMP\Paint.NET"){
-	Remove-Item -Recurse -Path "$env:TEMP\Paint.NET"
+	Expand-Archive -Path "C:\ProgramData\InstSys\$name\$setup" -DestinationPath "C:\ProgramData\InstSys\$name\extract"
+
+	Get-ChildItem "C:\ProgramData\InstSys\$name\extract" -Filter "*.msi" | Foreach-Object {
+		Start-Process -Wait -FilePath "msiexec" -ArgumentList "/qb","/i","$($_.FullName)","DESKTOPSHORTCUT=0"
 	}
-	New-Item -ItemType Directory -Path "$env:TEMP\Paint.NET"
-	Expand-Archive -Path "C:\ProgramData\InstSys\paintnet\$setup" -DestinationPath "$env:TEMP\Paint.NET"
-
-	Get-ChildItem "$env:TEMP\Paint.NET" -Filter "*.exe" | Foreach-Object {
-	$setupfile = $_.FullName
-	Start-Process -Wait -WorkingDirectory "$env:TEMP\Paint.NET" -FilePath "$setupfile" -ArgumentList "/createMsi","/auto"
-	}
-
-	Start-Process -Wait -FilePath "msiexec" -ArgumentList "/qb","/i","$env:USERPROFILE\Desktop\PaintDotNetMsi\PaintDotNet_x64.msi","DESKTOPSHORTCUT=0"
-
-	Remove-Item -Recurse "$env:TEMP\Paint.NET"
-	Remove-Item -Recurse "$env:USERPROFILE\Desktop\PaintDotNetMsi"
 
 } else {
 	Start-Process -FilePath "powershell" -ArgumentList "$PSScriptRoot\$($MyInvocation.MyCommand.Name)" -Wait -Verb RunAs
