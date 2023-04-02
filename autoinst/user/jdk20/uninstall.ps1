421e0fcba4c5e12b9d9b7ea5686eac579465c8f0
andreasgottardi <andreas.gottardi@gmx.at>
2023-04-02 22:14:45 +0200

    Adding JDK20.

--- autoinst/user/jdk20/uninstall.ps1
+++ autoinst/user/jdk20/uninstall.ps1
@@ -6,10 +6,10 @@ foreach($process in $processes){
 # Delete directories
 $TargetPath = "$env:LOCALAPPDATA\Programs\Java"
 
-Get-ChildItem -Path "$TargetPath" | Where-Object {$_.Name -match "(.*)jdk19(.*)"} | ForEach-Object {
+Get-ChildItem -Path "$TargetPath" | Where-Object {$_.Name -match "(.*)jdk20(.*)"} | ForEach-Object {
 	Remove-Item -Recurse -Force -Path "$($_.FullName)"
 }
 
 if("$env:JAVA_HOME_18" -ne ""){
-	[System.Environment]::SetEnvironmentVariable('JAVA_HOME_19', $null, [System.EnvironmentVariableTarget]::User)
+	[System.Environment]::SetEnvironmentVariable('JAVA_HOME_20', $null, [System.EnvironmentVariableTarget]::User)
 }
