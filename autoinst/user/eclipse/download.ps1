a7fcb38afdb0e9f6afa28f0eb3903ce6bf543f2e
andreasgottardi <andreas.gottardi@gmx.at>
2023-04-02 21:59:38 +0200

    Eclipse updated to latest version.

--- autoinst/user/eclipse/download.ps1
+++ autoinst/user/eclipse/download.ps1
@@ -9,48 +9,39 @@ Get-ChildItem -Path "$WorkDir" | ForEach-Object {
 $EclipseDir="$WorkDir\eclipse"
 
 $Repos = @(
-
-	# Regular Eclipse repositories
-	"https://download.eclipse.org/releases/2023-03",
-	"https://download.eclipse.org/eclipse/updates/4.27",
-	# "https://download.eclipse.org/e4/snapshots/org.eclipse.e4.ui",
-
-	# C/C++ repository
-	"https://download.eclipse.org/tools/cdt/releases/latest/",
-
-	#DBeaver repsitory
-	"https://dbeaver.io/update/git/latest/",
-	"https://dbeaver.io/update/office/latest/",
-	"https://dbeaver.io/update/latest/",
-
-	# EGit repository
-	"https://download.eclipse.org/egit/updates",
-
-	# Jaspersoft Studio repository
-	"http://jasperstudio.sourceforge.net/updates/",
-
-	# SpringBoot repository
-	"https://download.springsource.com/release/TOOLS/sts4/update/e4.27/",
-
-	# SonarLint repository
-	"https://eclipse-uc.sonarlint.org",
-
-	# Additional artifacts
-	"https://download.eclipse.org/tools/orbit/R-builds/R20210602031627/repository",
-	"https://download.eclipse.org/tools/orbit/R-builds/R20210602031627/repository",
-	"https://download.eclipse.org/releases/2022-03/202203161000",
-
-	# Wild Web Developer HTML, CSS, JSON, Yaml, JavaScript, TypeScript, Node tools
-	"http://download.eclipse.org/wildwebdeveloper/releases/latest/"
+    "https://download.eclipse.org/modeling/m2t/updates/",
+    "https://download.eclipse.org/modeling/m2t/xpand/updates/",
+    "https://download.eclipse.org/modeling/tmf/updates/",
+    "https://download.eclipse.org/usssdk/updates/release/latest",
+    "https://download.eclipse.org/wildwebdeveloper/releases/latest/",
+    "https://jasperstudio.sourceforge.net/updates/",
+    "https://dbeaver.io/update/git/latest/",
+    "https://dbeaver.io/update/latest/",
+    "https://dbeaver.io/update/office/latest/",
+    "https://download.eclipse.org/e4/snapshots/org.eclipse.e4.ui",
+    "https://download.eclipse.org/eclipse/updates/4.27",
+    "https://download.eclipse.org/egit/github/updates",
+    "https://download.eclipse.org/egit/github/updates-nightly",
+    "https://download.eclipse.org/egit/updates",
+    "https://download.eclipse.org/modeling/tmf/xtext/updates/composite/releases/",
+    "https://download.eclipse.org/releases/2022-03/202203161000",
+    "https://download.eclipse.org/releases/2023-03",
+    "https://download.eclipse.org/tools/cdt/releases/latest",
+    "https://download.eclipse.org/tools/gef/classic/releases/latest",
+    "https://download.eclipse.org/tools/orbit/downloads/drops/R20221123021534/repository",
+    "https://download.eclipse.org/tools/orbit/R-builds/R20210602031627/repository",
+    "https://download.springsource.com/release/TOOLS/sts4/update/e4.27/",
+    "https://eclipse-uc.sonarlint.org",
+    "https://jasperstudio.sourceforge.net/updates/"
 )
 
 $FeatureList = @(
 
 	# Jaspersoft studio
-	# "com.jaspersoft.studio.feature.feature.group",
-	# "net.sf.jasperreports.feature.feature.group",
-	# "net.sf.jasperreports.samples.feature.feature.group",
-	# "com.jaspersoft.studio.foundation.bundles.feature.group",
+	"com.jaspersoft.studio.feature.feature.group",
+	"net.sf.jasperreports.feature.feature.group",
+	"net.sf.jasperreports.samples.feature.feature.group",
+	"com.jaspersoft.studio.foundation.bundles.feature.group",
 
 	# Marketplace client
 	"org.eclipse.epp.mpc.feature.group",
@@ -65,16 +56,16 @@ $FeatureList = @(
 	"org.eclipse.cdt.feature.group",
 
 	# DBeaver database management
-	# "org.jkiss.dbeaver.debug.feature.feature.group",
-	# "org.jkiss.dbeaver.ui.feature.feature.group",
-	# "org.jkiss.dbeaver.git.feature.feature.group",
-	# "org.jkiss.dbeaver.ide.feature.feature.group",
-	# "org.jkiss.dbeaver.db.feature.feature.group",
-	# "org.jkiss.dbeaver.db.ui.feature.feature.group",
-	# "org.jkiss.dbeaver.ui.extra.feature.feature.group",
-	# "org.jkiss.dbeaver.runtime.feature.feature.group",
-	# "org.jkiss.dbeaver.ext.office.feature.feature.group",
-	# "org.jkiss.dbeaver.ext.ui.svg.feature.feature.group",
+	"org.jkiss.dbeaver.debug.feature.feature.group",
+	"org.jkiss.dbeaver.ui.feature.feature.group",
+	"org.jkiss.dbeaver.git.feature.feature.group",
+	"org.jkiss.dbeaver.ide.feature.feature.group",
+	"org.jkiss.dbeaver.db.feature.feature.group",
+	"org.jkiss.dbeaver.db.ui.feature.feature.group",
+	"org.jkiss.dbeaver.ui.extra.feature.feature.group",
+	"org.jkiss.dbeaver.runtime.feature.feature.group",
+	"org.jkiss.dbeaver.ext.office.feature.feature.group",
+	"org.jkiss.dbeaver.ext.ui.svg.feature.feature.group",
 
 	# Spring Tool Suite
 	"org.springframework.tooling.bosh.ls.feature.feature.group",
