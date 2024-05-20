194358c99b9433d2e7d12a97e85aa035c25acfe7
AndreasGottardi <andreas@goa.systems>
2024-05-20 09:36:12 +0200

    Adding 7-Zip installer.

---
+++ autoinst/system/7zip/uninstall.ps1
@@ -0,0 +1,8 @@
+if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+	if(Test-Path -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\7-Zip"){
+        $UninstallString = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\7-Zip").QuietUninstallString
+        Invoke-Expression "&${UninstallString}"
+    }
+} else {
+	Start-Process -FilePath "powershell" -ArgumentList "$PSScriptRoot\$($MyInvocation.MyCommand.Name)" -Wait -Verb RunAs
+}
