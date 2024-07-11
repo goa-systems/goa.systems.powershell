2caa87c03cf03ea644d88636396f392ab63370f3
AndreasGottardi <andreas@goa.systems>
2024-07-11 06:17:51 +0200

    Updating Netbeans installer.

--- autoinst/user/netbeans/install.ps1
+++ autoinst/user/netbeans/install.ps1
@@ -7,48 +7,41 @@ param (
 $Json = ConvertFrom-Json -InputObject (Get-Content -Path "$(${PSScriptRoot})\version.json" -Raw)
 $Version = $Json.version
 
-$FileName = "netbeans-$Version-bin.zip"
-$Url = "https://downloads.apache.org/netbeans/netbeans/$Version/$FileName"
+$FileName = "netbeans-${Version-bin}.zip"
+$Url = "https://downloads.apache.org/netbeans/netbeans/${Version}/${FileName}"
 
-if(Test-Path -Path "$InstallDir\$Version"){
+if(Test-Path -Path "${InstallDir}\${Version}"){
 	
 	Write-Host -Object "Defined version already installed."
 
 } else {
 
-	$DownloadDir = "$env:ProgramData\InstSys\netbeans"
+	$DownloadDir = "${env:TEMP}\$(New-Guid)"
 	
-	if( -Not (Test-Path -Path "$DownloadDir" )){
-		New-Item -Path "$DownloadDir" -ItemType "Directory"
+	if(Test-Path -Path "$DownloadDir"){
+		Remove-Item -Recurse -Force -Path "${DownloadDir}"
+	}
+	New-Item -Path "${DownloadDir}" -ItemType "Directory"
+	
+	if( -Not (Test-Path -Path "${InstallDir}" )){
+		New-Item -Path "${InstallDir}" -ItemType "Directory"
 	}
 	
-	if( -Not (Test-Path -Path "$InstallDir" )){
-		New-Item -Path "$InstallDir" -ItemType "Directory"
-	}
-	
-	if(Test-Path -Path "$env:TEMP\Netbeans"){
-		Remove-Item -Recurse -Force -Path "$env:TEMP\Netbeans"
-	}
-	New-Item -ItemType "Directory" -Path "$env:TEMP\Netbeans"
-	
-	if( -Not (Test-Path -Path "$DownloadDir\$FileName" )){
+	if( -Not (Test-Path -Path "${DownloadDir}\${FileName}" )){
 		$ProgressPreference = 'SilentlyContinue'
-		Invoke-WebRequest -Uri "$Url" -OutFile "$DownloadDir\$FileName"
+		Invoke-WebRequest -Uri "${Url}" -OutFile "${DownloadDir}\${FileName}"
 	}
 	
-	try { 7z | Out-Null } catch {}
-	if(-not ($?)){
-		Write-Host -Object "Can not find 7z.exe. Please check your global path and rerun installer."
-	} else {
-		Start-Process -FilePath "7z" -ArgumentList "x","-aos","-o`"$env:TEMP\Netbeans`"","-bb0","-bse0","-bsp2","-pdefault","-sccUTF-8","`"$DownloadDir\$FileName`"" -Wait -NoNewWindow
-	}
-	
-	<# Move extracted folder to the program installation directory. #>
-	Get-ChildItem -Path "$env:TEMP\Netbeans" | ForEach-Object {
-		
-	 	Move-Item -Path $_.FullName -Destination "$InstallDir\$Version"
+	Expand-Archive -Path "${DownloadDir}\${FileName}" -DestinationPath "${DownloadDir}"
+	Move-Item -Path "${DownloadDir}\netbeans" -Destination "${InstallDir}\${Version}"
 
-		 [System.Environment]::SetEnvironmentVariable("NETBEANS_HOME", "$InstallDir\$Version", [System.EnvironmentVariableTarget]::User)
-	}
+	<# Set version based environment variable. #>
+	Set-ItemProperty -Path "HKCU:\Environment" -Name "NETBEANS_HOME_${Version}" -Type ExpandString -Value "${InstallDir}\${Version}"
+
+	<# Point general environment variable to version based variable. #>
+	Set-ItemProperty -Path "HKCU:\Environment" -Name "NETBEANS_HOME"            -Type ExpandString -Value "%NETBEANS_HOME_22%"
+
+	<# Remove download directory. #>
+	Remove-Item -Recurse -Force -Path "${DownloadDir}"
 }
 
