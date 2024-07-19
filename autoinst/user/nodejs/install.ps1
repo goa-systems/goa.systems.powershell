94a4d41fb4f91b7accb7f78c8b420f7a0ab48bf0
AndreasGottardi <andreas@goa.systems>
2024-07-19 13:25:31 +0200

    Modifying NodeJS installer.

--- autoinst/user/nodejs/install.ps1
+++ autoinst/user/nodejs/install.ps1
@@ -6,28 +6,24 @@ param (
 $Version = (ConvertFrom-Json -InputObject (Get-Content -Raw -Path  "${PSScriptRoot}\version.json")).version
 $FileName = "node-$Version-win-x64.zip"
 $DownloadUrl = "https://nodejs.org/dist/$Version/$FileName"
-$AppName = "nodejs"
 
-$DownloadDir = "$env:ProgramData\InstSys\$AppName"
+$DownloadDir = "$env:TEMP\$(New-Guid)"
 
 if(-not (Test-Path -Path "$InstallDir")){
 	New-Item -ItemType "Directory" -Path "$InstallDir"
 }
-if(Test-Path -Path "$env:TEMP\$AppName"){
-	Remove-Item -Recurse -Force -Path "$env:TEMP\$AppName"
+if(Test-Path -Path "$DownloadDir"){
+	Remove-Item -Recurse -Force -Path "$DownloadDir"
 }
-New-Item -ItemType "Directory" -Path "$env:TEMP\$AppName"
-
-if(-not (Test-Path -Path "$DownloadDir")){
-	New-Item -ItemType "Directory" -Path "$DownloadDir"
-}
+New-Item -ItemType "Directory" -Path "$DownloadDir"
 
 $ProgressPreference = 'SilentlyContinue'
 Invoke-WebRequest -Uri "$DownloadUrl" -OutFile "$DownloadDir\$FileName"
 
-Expand-Archive -Path "$DownloadDir\$FileName" -DestinationPath "$env:TEMP\$AppName"
+Expand-Archive -Path "$DownloadDir\$FileName" -DestinationPath "$DownloadDir"
+Remove-Item -Force -Path "$DownloadDir\$FileName"
 
-Get-ChildItem -Path "$env:TEMP\$AppName" | ForEach-Object {
+Get-ChildItem -Path "$DownloadDir" | ForEach-Object {
 	Move-Item -Path $_.FullName -Destination "$InstallDir"
 	[System.Environment]::SetEnvironmentVariable("NODEJS_HOME", "$($InstallDir)\$($_.Name)", [System.EnvironmentVariableTarget]::User)
 }
