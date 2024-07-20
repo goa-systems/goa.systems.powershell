686883f21f8fd3f535cd8f7571e1d9f7ddcf9c10
AndreasGottardi <andreas@goa.systems>
2024-07-20 22:49:30 +0200

    Updating Libreoffice installer.

--- autoinst/system/libreoffice/install.ps1
+++ autoinst/system/libreoffice/install.ps1
@@ -1,24 +1,22 @@
 if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
 	Set-Location -Path "$PSScriptRoot"
 	$Json = Get-Content -Raw -Path "version.json" | ConvertFrom-Json
-	$name = "libreoffice"
-	$version = $Json.version
-	$setup = "LibreOffice_${version}_Win_x86-64.msi"
-	$dlurl = "https://ftp.gwdg.de/pub/tdf/libreoffice/stable/$version/win/x86_64/$setup"
+	$Version = $Json.version
+	$SetupFile = "LibreOffice_${Version}_Win_x86-64.msi"
+	$DownloadUrl = "https://ftp.gwdg.de/pub/tdf/libreoffice/stable/${Version}/win/x86_64/${SetupFile}"
+	$DownloadDir = "$env:TEMP\$(New-Guid)"
 
-	If (-Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\$name")) {
-		New-Item -Path "$env:SystemDrive\ProgramData\InstSys\$name" -ItemType "Directory"
+	If (Test-Path -Path "${DownloadDir}") {
+		Remove-Item -Recurse -Force -Path "${DownloadDir}"
 	}
+	New-Item -Path "${DownloadDir}" -ItemType "Directory"
 
-	<# Download if setup is not found in execution path. #>
-	if ( -Not (Test-Path -Path "$env:SystemsDrive\ProgramData\InstSys\$name\$setup")) {
-		Write-Host "Downloading from $dlurl"
-		Start-BitsTransfer `
-			-Source $dlurl `
-			-Destination "$env:SystemDrive\ProgramData\InstSys\$name\$setup"
-	}
-
-	Start-Process -Wait -FilePath "msiexec" -ArgumentList "/qb", "/i", "$env:SystemDrive\ProgramData\InstSys\$name\$setup", "REGISTER_ALL_MSO_TYPES=1", "INSTALLLOCATION=`"C:\Program Files\LibreOffice`"", "ALLUSERS=1", "CREATEDESKTOPLINK=0", "ADDLOCAL=ALL", "UI_LANGS=en_US,de", "ADDLOCAL=ALL", "REMOVE=gm_o_Quickstart,gm_o_Activexcontrol"
+	$ProgressPreference = "SilentlyContinue"
+	Write-Host -Object "Downloading ..."
+	Invoke-WebRequest -Uri "${DownloadUrl}" -OutFile "${DownloadDir}\${SetupFile}"
+	Write-Host -Object "Download finished."
+	Start-Process -Wait -FilePath "msiexec" -ArgumentList "/qb", "/i", "${DownloadDir}\${SetupFile}", "REGISTER_ALL_MSO_TYPES=1", "INSTALLLOCATION=`"C:\Program Files\LibreOffice`"", "ALLUSERS=1", "CREATEDESKTOPLINK=0", "ADDLOCAL=ALL", "UI_LANGS=en_US,de", "ADDLOCAL=ALL", "REMOVE=gm_o_Quickstart,gm_o_Activexcontrol"
+	Remove-Item -Recurse -Force -Path "${DownloadDir}"
 }
 else {
 	Start-Process -FilePath "powershell" -ArgumentList "$PSScriptRoot\$($MyInvocation.MyCommand.Name)" -Wait -Verb RunAs
