81273c5047e5034c3b1a517d7b2f27f3c9df99ee
AndreasGottardi <andreas@goa.systems>
2024-08-20 20:36:54 +0200

    Removing faulty uninstaller.

--- autoinst/user/dotnet/uninstall.ps1
+++ autoinst/user/dotnet/uninstall.ps1
@@ -1,7 +1,2 @@
 Remove-Item -Recurse -Force -Path "${env:LOCALAPPDATA}\Programs\.NET"
-$Path = [System.Environment]::GetEnvironmentVariable('PATH', [System.EnvironmentVariableTarget]::User)
-$DotnetHome = [System.Environment]::GetEnvironmentVariable('DOTNET_HOME', [System.EnvironmentVariableTarget]::User)
-$DotnetHome =  $DotnetHome -replace "\\","\\"
-$Path = ($Path.Split(';') | Where-Object { $_ -notmatch "$DotnetHome" }) -join ';'
-[System.Environment]::SetEnvironmentVariable('PATH', $Path, [System.EnvironmentVariableTarget]::User)
-[System.Environment]::SetEnvironmentVariable('DOTNET_HOME', $null, [System.EnvironmentVariableTarget]::User)
+Write-Host -Object "Please remove %DOTNET_HOME% manually from path."
