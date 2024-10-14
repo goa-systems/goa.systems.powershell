2f77f3b6ef662cbcfe7d33902160683cfef37d0f
Andreas Gottardi <andreas@goa.systems>
2024-10-14 22:52:33 +0200

    Updating and improving Inkscape installer.

--- autoinst/system/inkscape/install.ps1
+++ autoinst/system/inkscape/install.ps1
@@ -1,24 +1,17 @@
 if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
 	Set-Location -Path "$PSScriptRoot"
 	$Json = Get-Content -Raw -Path "version.json" | ConvertFrom-Json
-	$name = "inkscape"
-	$setup = "inkscape-$($Json.version)_$($Json.date)_$($Json.hash)-x64.msi"
-	$dlurl = "https://media.inkscape.org/dl/resources/file/$setup"
-	
-	If(-Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\$name")){
-		New-Item -Path "$env:SystemDrive\ProgramData\InstSys\$name" -ItemType "Directory"
+	$TempDir = "${env:TEMP}\$(New-Guid)"
+	$SetupFile = "inkscape-$($Json.version)_$($Json.date)_$($Json.hash)-x64.msi"
+	$DownloadUrl = "https://media.inkscape.org/dl/resources/file/$SetupFile"
+	If(Test-Path -Path "${TempDir}"){
+		Remove-Item -Recurse -Force -Path "${TempDir}"
 	}
-	
+	New-Item -Path "${TempDir}" -ItemType "Directory"
 	<# Download if setup is not found in execution path. #>
-	if( -Not (Test-Path -Path "$env:SystemDrive\ProgramData\InstSys\$name\$setup")){
-		Write-Host "Downloading from $dlurl"
-		Start-BitsTransfer `
-		-Source $dlurl `
-		-Destination "$env:SystemDrive\ProgramData\InstSys\$name\$setup"
-	}
-	
-	Start-Process -Wait -FilePath "msiexec" -ArgumentList "/qb","/i","$env:SystemDrive\ProgramData\InstSys\$name\$setup","INSTALLLOCATION=`"$env:ProgramFiles\Inkscape`""
-	Copy-Item -Path ".\default.svg" -Destination "$env:ProgramFiles\Inkscape\share\templates\default.svg"
+	Write-Host "Downloading from ${DownloadUrl}"
+	Start-BitsTransfer -Source "${DownloadUrl}" -Destination "${TempDir}\$SetupFile"
+	Start-Process -Wait -FilePath "msiexec" -ArgumentList @("/qb","/i","${TempDir}\${SetupFile}","INSTALLLOCATION=`"$env:ProgramFiles\Inkscape`"")
 } else {
 	$curscriptname = $MyInvocation.MyCommand.Name 
 	Start-Process -FilePath "pwsh.exe" -ArgumentList "$PSScriptRoot\$curscriptname" -Verb RunAs
--- autoinst/system/inkscape/update.ps1
+++ autoinst/system/inkscape/update.ps1
@@ -1,6 +1,7 @@
 if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
 	Set-Location -Path "$PSScriptRoot"
-	Start-Process -FilePath "pwsh.exe" -ArgumentList ".\uninstall.ps1;.\install.ps1"
+	Start-Process -FilePath "pwsh.exe" -ArgumentList @("-File", ".\uninstall.ps1") -Wait
+	Start-Process -FilePath "pwsh.exe" -ArgumentList @("-File", ".\install.ps1") -Wait
 } else {
 	Start-Process -FilePath "pwsh.exe" -ArgumentList "$PSScriptRoot\$($MyInvocation.MyCommand.Name)" -Verb RunAs
 }
