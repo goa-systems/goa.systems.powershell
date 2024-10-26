e040321952696955124cea6f60cfda36b067ef2d
AndreasGottardi <andreas@goa.systems>
2024-10-26 18:11:52 +0200

    Extending dynamic JDK installation.

--- autoinst/user/jdk23/Install-Dynamic.ps1
+++ autoinst/user/jdk23/Install-Dynamic.ps1
@@ -14,14 +14,66 @@ function Convert-VersionArray {
     return $DistroVersion
 }
 
-$ApiRequestUrl = "https://api.azul.com/metadata/v1/zulu/packages/?java_version=23&os=windows&arch=x64&archive_type=zip&java_package_type=jdk&javafx_bundled=true&distro_version=23&release_status=ga&availability_types=CA&certifications=tck&latest=true"
-$Release = (Invoke-RestMethod -Uri "${ApiRequestUrl}")
+function Get-Package {
+    param (
+        # The download url
+        [Parameter(Mandatory=$true)]
+        [string]
+        $DownloadUrl,
 
-$DistVersion = (Convert-VersionArray -VersionArray $Release.distro_version)
-$JavaVersion = (Convert-VersionArray -VersionArray $Release.java_version)
+        # The destination file
+        [Parameter(Mandatory=$true)]
+        [string]
+        $OutFile
+    )
 
-$ProgressPreference = "SilentlyContinue"
-Write-Host -Object "Download started."
-Invoke-WebRequest -Uri "$($Release.download_url)" -OutFile "${env:TEMP}\zulu${DistVersion}-ca-fx-jdk${JavaVersion}-win_x64.zip"
-Write-Host -Object "Download finished"
-$ProgressPreference = "Continue"
+    $ProgressPreference = "SilentlyContinue"
+    Write-Host -Object "Download started."
+    Invoke-WebRequest -Uri "${DownloadUrl}" -OutFile "${OutFile}"
+    Write-Host -Object "Download finished"
+    $ProgressPreference = "Continue"
+}
+
+function New-TemporaryDirectory {
+    $TempPath = "${env:TEMP}\$(New-Guid)"
+    if (Test-Path -Path "${TempPath}") {
+        Remove-Item -Recurse -Force "${TempPath}"
+    }
+    New-Item -ItemType "Directory" -Path "${TempPath}"
+}
+
+function Install-Java {
+    param (
+        # Java version 23, 21, 17, ...
+        [Parameter(Mandatory=$true)]
+        [string]
+        $JavaMajorVersion
+    )
+
+    $ApiRequestUrl = "https://api.azul.com/metadata/v1/zulu/packages/?java_version=${JavaMajorVersion}&os=windows&arch=x64&archive_type=zip&java_package_type=jdk&javafx_bundled=true&distro_version=${JavaMajorVersion}&release_status=ga&availability_types=CA&certifications=tck&latest=true"
+    
+    # Can return multiple releases. Always take the latest on index 0.
+    $Release = (Invoke-RestMethod -Uri "${ApiRequestUrl}")[0]
+
+    $DistVersion = (Convert-VersionArray -VersionArray $Release.distro_version)
+    $JavaVersion = (Convert-VersionArray -VersionArray $Release.java_version)
+
+    $DownloadTargetFile = "${env:TEMP}\zulu${DistVersion}-ca-fx-jdk${JavaVersion}-win_x64.zip"
+    Get-Package -DownloadUrl "$($Release.download_url)" -OutFile "${DownloadTargetFile}"
+    $TemporaryDirectory
+
+    $TemporaryDirectory = (New-TemporaryDirectory).FullName
+
+    Expand-Archive -Path "${DownloadTargetFile}" -DestinationPath "${TemporaryDirectory}"
+
+    Get-ChildItem -Path "${TemporaryDirectory}" | ForEach-Object {
+        $Destination = "${env:LOCALAPPDATA}\Programs\Java\$($_.Name)"
+        Move-Item -Path "$($_.FullName)" -Destination "${Destination}"
+        [System.Environment]::SetEnvironmentVariable("JAVA_HOME_${JavaMajorVersion}","${Destination}", [System.EnvironmentVariableTarget]::User)
+    }
+
+    Remove-Item -Force -Path "${TemporaryDirectory}" -Recurse
+    Remove-Item -Force -Path "${env:TEMP}\zulu${DistVersion}-ca-fx-jdk${JavaVersion}-win_x64.zip"
+}
+
+@("21","17","11","8") | ForEach-Object { Install-Java -JavaMajorVersion "$($_)" }
