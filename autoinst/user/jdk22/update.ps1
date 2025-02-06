813dccc5fd9909e92728c1982f464efaf28c9410
AndreasGottardi <andreas@goa.systems>
2025-02-06 20:03:50 +0100

    Removing legacy JDK updaters.

--- autoinst/user/jdk22/update.ps1
+++
@@ -1,3 +0,0 @@
-Set-Location -Path "$PSScriptRoot"
-Start-Process -Wait -FilePath "pwsh.exe" -ArgumentList "-File","uninstall.ps1"
-Start-Process -Wait -FilePath "pwsh.exe" -ArgumentList "-File","install.ps1"
