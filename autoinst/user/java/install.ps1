05929bd43a6ece8e20f1b40b0713888c8572397c
AndreasGottardi <andreas@goa.systems>
2025-02-11 10:39:12 +0100

    Removing legacy Java install wrapper.

--- autoinst/user/java/install.ps1
+++
@@ -1,6 +0,0 @@
-Start-Process -FilePath "pwsh" -ArgumentList @("-File", "$PSScriptRoot\..\jdk8\install.ps1") -Wait
-Start-Process -FilePath "pwsh" -ArgumentList @("-File", "$PSScriptRoot\..\jdk11\install.ps1") -Wait
-Start-Process -FilePath "pwsh" -ArgumentList @("-File", "$PSScriptRoot\..\jdk17\install.ps1") -Wait
-Start-Process -FilePath "pwsh" -ArgumentList @("-File", "$PSScriptRoot\..\jdk18\install.ps1") -Wait
-
-[System.Environment]::SetEnvironmentVariable('JAVA_HOME',"$env:JAVA_HOME_18", [System.EnvironmentVariableTarget]::User)
