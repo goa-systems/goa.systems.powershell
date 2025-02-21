8258ba559f83c7822c4096db12f485a46a65150a
Andreas Gottardi <andreas.gottardi@gmx.at>
2025-02-21 08:48:10 +0100

    Removing JDK parent directory during uninstall.

--- autoinst/user/jdk/Uninstall.ps1
+++ autoinst/user/jdk/Uninstall.ps1
@@ -5,3 +5,6 @@ Get-ChildItem -Path "Env:\" | Where-Object {$_.Name -match "JAVA_HOME_.*" } | Fo
     }
     [System.Environment]::SetEnvironmentVariable("$($_.Name)","", [System.EnvironmentVariableTarget]::User)
 }
+
+Write-Host -Object "Cleaning up parent directoy"
+Remove-Item -Force -Recurse -Path "${env:LOCALAPPDATA}\Programs\Java"
