4624a31facf7e5ca59ccbb572b000dc31a534037
AndreasGottardi <andreas@goa.systems>
2025-04-02 15:47:35 +0200

    Adding temporary directory removal.

--- autoinst/system/putty/install.ps1
+++ autoinst/system/putty/install.ps1
@@ -7,20 +7,18 @@ if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsI
 	$SetupFile = "putty-64bit-$Version-installer.msi"
 	$DownloadUrl = "https://the.earth.li/~sgtatham/putty/$Version/w64/$SetupFile"
 	$DownloadDir = "${env:TEMP}\$(New-Guid)"
-	
+
 	If(Test-Path -Path "${DownloadDir}"){
 		Remove-Item -Recurse -Force -Path "${DownloadDir}"
 	}
 	New-Item -Path "${DownloadDir}" -ItemType "Directory"
-	
-	<# Download putty, if setup is not found in execution path. #>
 	if( -Not (Test-Path -Path "${DownloadDir}\$SetupFile")){
 		Start-BitsTransfer `
 		-Source "$DownloadUrl" `
 		-Destination "${DownloadDir}\$SetupFile"
 	}
-	
 	Start-Process -Wait -FilePath "msiexec" -ArgumentList "/qb","/i","${DownloadDir}\${SetupFile}","/passive","INSTALLDIR=`"C:\Program Files\Putty`"","ADDLOCAL=FilesFeature,PathFeature,PPKFeature"	
+	Remove-Item -Recurse -Force -Path "${DownloadDir}"
 } else {
 	Start-Process -FilePath "pwsh.exe" -ArgumentList "$PSScriptRoot\$($MyInvocation.MyCommand.Name)" -Wait -Verb RunAs
 }
