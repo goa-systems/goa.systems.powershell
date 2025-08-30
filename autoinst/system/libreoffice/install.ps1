c824ab67e5067d3c234483f45f65bb3cd4cd847a
Andreas Gottardi <andreas@goa.systems>
2025-08-30 14:00:31 +0200

    Removing version.json.
    Parsing version directly.

--- autoinst/system/libreoffice/install.ps1
+++ autoinst/system/libreoffice/install.ps1
@@ -1,9 +1,12 @@
 if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
 	Set-Location -Path "$PSScriptRoot"
-	$Json = Get-Content -Raw -Path "version.json" | ConvertFrom-Json
-	$Version = $Json.version
+	
+	$BaseUrl = "https://download.documentfoundation.org/libreoffice/stable/"
+	$Response = Invoke-WebRequest -Uri $BaseUrl
+	$Versions = $Response.Links | Where-Object { $_.href -match '^\d' } | ForEach-Object { $_.href.TrimEnd('/') }
+	$Version = $Versions | Sort-Object {[version]$_} | Select-Object -Last 1
 	$SetupFile = "LibreOffice_${Version}_Win_x86-64.msi"
-	$DownloadUrl = "https://ftp.gwdg.de/pub/tdf/libreoffice/stable/${Version}/win/x86_64/${SetupFile}"
+	$DownloadUrl = "${BaseUrl}${Latest}/win/x86_64/${SetupFile}"
 	$DownloadDir = "$env:TEMP\$(New-Guid)"
 
 	If (Test-Path -Path "${DownloadDir}") {
