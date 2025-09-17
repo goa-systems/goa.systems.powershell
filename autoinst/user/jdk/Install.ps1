0d0367d8723fa8524f830dbb09c988490351b01f
Andreas Gottardi <andreas@goa.systems>
2025-09-17 07:28:51 +0200

    Updating JDK installer to version 25.

--- autoinst/user/jdk/Install.ps1
+++ autoinst/user/jdk/Install.ps1
@@ -1,3 +1,3 @@
 . "${PSScriptRoot}\Functions.ps1"
 
-@("24","21","17","11","8") | ForEach-Object { Install-Java -JavaMajorVersion "$($_)" }
+@("25","21","17","11","8") | ForEach-Object { Install-Java -JavaMajorVersion "$($_)" }
