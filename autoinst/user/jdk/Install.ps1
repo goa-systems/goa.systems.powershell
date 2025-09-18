f3cd056def4936c52257719834fd84c2e4843693
Andreas Gottardi <andreas@goa.systems>
2025-09-18 08:40:41 +0200

    Changing JDK installer.

--- autoinst/user/jdk/Install.ps1
+++ autoinst/user/jdk/Install.ps1
@@ -1,3 +1,8 @@
 . "${PSScriptRoot}\Functions.ps1"
 
-@("25","21","17","11","8") | ForEach-Object { Install-Java -JavaMajorVersion "$($_)" }
+$VersionsToInstall = @("25", "24" ,"21","17","11","8")
+
+$VersionsToInstall | ForEach-Object { Install-Java -JavaMajorVersion "$($_)" }
+
+$env:JAVA_HOME = "%JAVA_HOME_$($VersionsToInstall[0])%"
+Set-ItemProperty -Path "HKCU:\Environment" -Name "JAVA_HOME" -Value "${env:JAVA_HOME}" -Type "ExpandString"
--- autoinst/user/jdk/Uninstall.ps1
+++ autoinst/user/jdk/Uninstall.ps1
@@ -3,8 +3,10 @@ Get-ChildItem -Path "Env:\" | Where-Object {$_.Name -match "JAVA_HOME_.*" } | Fo
     if (Test-Path -Path "${InstallPath}"){
         Remove-Item -Force -Recurse -Path "${InstallPath}"
     }
-    [System.Environment]::SetEnvironmentVariable("$($_.Name)","", [System.EnvironmentVariableTarget]::User)
+    [System.Environment]::SetEnvironmentVariable("$($_.Name)", [NullString]::Value, [System.EnvironmentVariableTarget]::User)
 }
+
+[System.Environment]::SetEnvironmentVariable("JAVA_HOME", [NullString]::Value, [System.EnvironmentVariableTarget]::User)
 
 Write-Host -Object "Cleaning up parent directoy"
 Remove-Item -Force -Recurse -Path "${env:LOCALAPPDATA}\Programs\Java"
