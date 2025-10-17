c746508c113d91a25ba35a397fe7f9bd7c38b94c
Andreas Gottardi <andreas@goa.systems>
2025-10-17 21:37:12 +0200

    Removing version.json from Gimp installer.

--- autoinst/system/gimp/install.ps1
+++ autoinst/system/gimp/install.ps1
@@ -1,11 +1,36 @@
 if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-	
+
+	function Get-LatestVersion {
+		$VersionsUrl = "https://www.gimp.org/gimp_versions.json"
+		$VersionsObj = Invoke-RestMethod -Uri "${VersionsUrl}" -Method "GET"
+		$LatestDate = Get-Date "1970-01-01"
+		$Versions = @("","")
+
+		$VersionsObj.STABLE | ForEach-Object {
+			$Obj = $_
+			$Obj.windows | ForEach-Object {
+				if (-Not ([String]::IsNullOrEmpty($_.date))) {
+					$Date = Get-Date $_.date
+					if ($Date -gt $LatestDate) {
+						$LatestDate = $Date
+						$MinorVersion = $Obj.version
+						$MinorVersion = $MinorVersion -split "\."
+						$MinorVersion = "v$($MinorVersion[0]).$($MinorVersion[1])"
+						$Versions[0] = $MinorVersion
+						$Versions[1] = $_.filename
+					}
+				}
+			}
+		}
+		return $Versions
+	}
+
 	Set-Location -Path "$PSScriptRoot"
-	$Json = Get-Content -Raw -Path "version.json" | ConvertFrom-Json
-	$MinorVersion = $Json.minorversion
-	$SetupFile = $Json.setupfile
+	$VersArr = Get-LatestVersion
+	$MinorVersion = $VersArr[0]
+	$SetupFile = $VersArr[1]
 	$DownloadDir = "${env:TEMP}\$(New-Guid)"
-	if (Test-Path -Path "${DownloadDir}"){
+	if (Test-Path -Path "${DownloadDir}") {
 		Remove-Item -Recurse -Force -Path "${DownloadDir}"
 	}
 	New-Item -ItemType "Directory" -Path "${DownloadDir}"
@@ -15,6 +40,7 @@ if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsI
 	Write-Host -Object "Download finished."
 	Start-Process -Wait -FilePath "${DownloadDir}\${SetupFile}" -ArgumentList @("/SILENT", "/MERGETASKS=`"!desktopicon`"", "/NORESTART", "/ALLUSERS", "/DIR=`"${env:PROGRAMFILES}\Gimp`"")
 	Remove-Item -Recurse -Force -Path "${DownloadDir}"
-} else {
+}
+else {
 	Start-Process -FilePath "pwsh.exe" -ArgumentList "$PSScriptRoot\$($MyInvocation.MyCommand.Name)" -Wait -Verb RunAs
 }
