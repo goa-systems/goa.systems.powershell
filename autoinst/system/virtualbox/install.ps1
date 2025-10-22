3bf4d76572847aa28fbf3ade1671decda3e03967
AndreasGottardi <andreas@goa.systems>
2025-10-22 15:07:51 +0200

    Removing version.json from installer.

--- autoinst/system/virtualbox/install.ps1
+++ autoinst/system/virtualbox/install.ps1
@@ -1,8 +1,23 @@
 if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+	
+	function Get-Version {
+		$LatestVersion = Invoke-WebRequest -Uri "https://download.virtualbox.org/virtualbox/LATEST-STABLE.TXT"
+		$LatestVersion = "${LatestVersion}".Trim()
+		$FilesPage = Invoke-WebRequest -Uri "https://download.virtualbox.org/virtualbox/${LatestVersion}/"
+		$VersionData = $null
+		if ("$FilesPage" -match '<a href="Oracle_VirtualBox_Extension_Pack-(.*)\.vbox-extpack">.*<\/a>') {
+			$VD = $Matches.1 -split "-"
+			$VersionData = @{
+				Version  = $VD[0];
+				Revision = $VD[1]
+			}
+		}
+		return $VersionData
+	}
+	
 	Set-Location -Path "$PSScriptRoot"
-	$Json = Get-Content -Raw -Path "version.json" | ConvertFrom-Json
-	$vboxvers = $Json.version
-	$vboxversrev = "$vboxvers-$($Json.revision)"
+	$VersionData = Get-Version
+	$vboxversrev = "$($VersionData.Version)-$($VersionData.Revision)"
 	$vboxsetup = "VirtualBox-$vboxversrev-Win.exe"
 	$vboxexpacksetup = "Oracle_VM_VirtualBox_Extension_Pack-$vboxvers.vbox-extpack"
 
@@ -27,16 +42,17 @@ if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsI
 	Start-Process -FilePath "$env:SystemDrive\ProgramData\InstSys\virtualbox\$vboxsetup" -ArgumentList "--extract", "--path", "`"$env:TEMP\VBox`"", "--silent" -Wait
 	
 	Get-ChildItem -Path "$env:TEMP\VBox" | ForEach-Object {
-		if($_.FullName.EndsWith("msi")){
+		if ($_.FullName.EndsWith("msi")) {
 			$msipath = $_.FullName;
 			[System.IO.File]::WriteAllBytes("$env:TEMP\VBox\certificate.cer", ((Get-AuthenticodeSignature "$msipath").SignerCertificate).Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Cert));
 			Import-Certificate -FilePath "$env:TEMP\VBox\certificate.cer" -CertStoreLocation "Cert:\LocalMachine\TrustedPublisher"
-			Start-Process -FilePath "msiexec.exe" -ArgumentList "/package","$msipath","/passive","/norestart","ADDLOCAL=VBoxApplication,VBoxUSB,VBoxNetworkFlt,VBoxNetworkAdp","NETWORKTYPE=NDIS6","VBOX_INSTALLDESKTOPSHORTCUT=0","VBOX_REGISTERFILEEXTENSIONS=0" -Wait
+			Start-Process -FilePath "msiexec.exe" -ArgumentList "/package", "$msipath", "/passive", "/norestart", "ADDLOCAL=VBoxApplication,VBoxUSB,VBoxNetworkFlt,VBoxNetworkAdp", "NETWORKTYPE=NDIS6", "VBOX_INSTALLDESKTOPSHORTCUT=0", "VBOX_REGISTERFILEEXTENSIONS=0" -Wait
 		}
 	}
 	
-	Start-Process -FilePath "$env:ProgramFiles\Oracle\VirtualBox\VBoxManage.exe" -ArgumentList "extpack","install","$env:SystemDrive\ProgramData\InstSys\virtualbox\$vboxexpacksetup","--replace","--accept-license=33d7284dc4a0ece381196fda3cfe2ed0e1e8e7ed7f27b9a9ebc4ee22e24bd23c" -Wait
+	Start-Process -FilePath "$env:ProgramFiles\Oracle\VirtualBox\VBoxManage.exe" -ArgumentList "extpack", "install", "$env:SystemDrive\ProgramData\InstSys\virtualbox\$vboxexpacksetup", "--replace", "--accept-license=33d7284dc4a0ece381196fda3cfe2ed0e1e8e7ed7f27b9a9ebc4ee22e24bd23c" -Wait
 	Remove-Item -Recurse -Path "$env:TEMP\VBox"
-} else {
+}
+else {
 	Start-Process -FilePath "pwsh.exe" -ArgumentList "$PSScriptRoot\$($MyInvocation.MyCommand.Name)" -Wait -Verb RunAs
 }
