55f49b0c4138b8d22151a52b6017d764297da662
AndreasGottardi <andreas@goa.systems>
2025-10-22 10:08:41 +0200

    Removing version.json from PHP installer.

--- autoinst/user/php/install.ps1
+++ autoinst/user/php/install.ps1
@@ -1,22 +1,38 @@
+function Get-Version {
+	$VersionData = Invoke-RestMethod -Uri "https://windows.php.net/downloads/releases/releases.json"
+	$HighestVersion = 0.0
+	$PackageType = ""
+	$VersionData.psobject.properties.name | ForEach-Object {
+		if ($_ -gt $HighestVersion) {
+			$HighestVersion = $_
+		}
+	}
+	$VersionData.$HighestVersion.psobject.Properties.name | ForEach-Object {
+		if($_ -match '^ts-vs\d+-x64$'){
+			$PackageType += $_
+		}
+	}
+	return $VersionData.$HighestVersion.$PackageType.zip.path
+}
+
 Set-Location -Path "$PSScriptRoot"
 
-$Version = (Get-Content -Path "version.json" -Raw | ConvertFrom-Json).version
-$PhpZip = "php-$Version-Win32-vs16-x64.zip"
-$TmpDir = "$env:TEMP\$(New-Guid)"
-$TargetDir = "$env:LocalAppData\Programs\Php\$Version"
+$PhpZip = Get-Version
+$TmpDir = "${env:TEMP}\$(New-Guid)"
+$TargetDir = "${env:LocalAppData}\Programs\Php\${Version}"
 
 New-Item -ItemType "Directory" -Path "$TmpDir"
 
-if( -not (Test-Path "$TargetDir")){
-	New-Item -ItemType "Directory" -Path "$TargetDir"
+if( -not (Test-Path "${TargetDir}")){
+	New-Item -ItemType "Directory" -Path "${TargetDir}"
 }
 
 $ProgressPreference = 'SilentlyContinue'
-$PhpDownloadUrl = "https://windows.php.net/downloads/releases/$PhpZip"
-Write-Host -Object "Starting php download from $PhpDownloadUrl"
-Invoke-WebRequest -Uri $PhpDownloadUrl -OutFile "$TmpDir\$PhpZip"
+$PhpDownloadUrl = "https://windows.php.net/downloads/releases/${PhpZip}"
+Write-Host -Object "Starting php download from ${PhpDownloadUrl}"
+Invoke-WebRequest -Uri ${PhpDownloadUrl} -OutFile "${TmpDir}\${PhpZip}"
 Write-Host -Object "Php download done."
 
-Expand-Archive -Path "$TmpDir\$PhpZip" -DestinationPath "$TargetDir"
+Expand-Archive -Path "${TmpDir}\${PhpZip}" -DestinationPath "${TargetDir}"
 
-[System.Environment]::SetEnvironmentVariable("PHP_HOME", "$TargetDir", [System.EnvironmentVariableTarget]::User)
+[System.Environment]::SetEnvironmentVariable("PHP_HOME", "${TargetDir}", [System.EnvironmentVariableTarget]::User)
