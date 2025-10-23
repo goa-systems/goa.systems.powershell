59e2fea360397df3806a6741db68eff46904d32d
Andreas Gottardi <andreas.gottardi@gmx.at>
2025-10-23 09:15:12 +0200

    Adding explorer restart.

--- autoinst/user/jdk/Install.ps1
+++ autoinst/user/jdk/Install.ps1
@@ -5,4 +5,7 @@ $VersionsToInstall = @("25", "24" ,"21","17","11","8")
 $VersionsToInstall | ForEach-Object { Install-Java -JavaMajorVersion "$($_)" }
 
 $env:JAVA_HOME = "%JAVA_HOME_$($VersionsToInstall[0])%"
+
 Set-ItemProperty -Path "HKCU:\Environment" -Name "JAVA_HOME" -Value "${env:JAVA_HOME}" -Type "ExpandString"
+
+Stop-Process -Name "explorer"
