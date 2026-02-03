7b3d7169b236705684d5155dd69bf1315b165bc0
Andreas Gottardi <andreas@goa.systems>
2026-02-03 21:16:57 +0100

    Switching to automatic Python version evaluation.

--- autoinst/user/python/install.ps1
+++ autoinst/user/python/install.ps1
@@ -1,20 +1,29 @@
-$Version = (Get-Content -Path "$PSScriptRoot\version.json" | ConvertFrom-Json).version
-$OutFile = "python-$Version-amd64.exe"
-$DownloadUrl = "https://www.python.org/ftp/python/$Version/$OutFile"
+function Get-Installer {
+    $ProgressPreference = "SilentlyContinue"
+    $DownloadLink = ""
+    (Invoke-WebRequest -Uri "https://www.python.org/downloads/").Links | ForEach-Object {
+        if ($_.href -match "https:\/\/www.python.org\/ftp\/python/\d*\.\d*\.\d*\/Python-\d*\.\d*\.\d*\-amd64\.exe") {
+            $DownloadLink += $_.href
+        }
+    }
+    return $DownloadLink
+}
+
+$DownloadUrl = Get-Installer
 
 $DownloadDir = "${env:TEMP}\$(New-Guid)"
 
-if(-not (Test-Path -Path "$DownloadDir")){
-	New-Item -ItemType "Directory" -Path "$DownloadDir"
+if(Test-Path -Path "$DownloadDir"){
+	Remove-Item -Force -Recurse -Path "$DownloadDir"
 }
+New-Item -ItemType "Directory" -Path "$DownloadDir"
 
 $ProgressPreference = 'SilentlyContinue'
-if(-not (Test-Path -Path "$DownloadDir\$OutFile")){
-	Invoke-WebRequest -Uri "$DownloadUrl" -OutFile "$DownloadDir\$OutFile"
-}
+Invoke-WebRequest -Uri "${DownloadUrl}" -OutFile "${DownloadDir}\python-installer.exe"
+
 
 Write-Host -Object "Installing python."
-Start-Process -FilePath "$DownloadDir\$OutFile" -Wait -ArgumentList @(`
+Start-Process -FilePath "${DownloadDir}\python-installer.exe" -Wait -ArgumentList @(`
 	"/passive",`
 	"TargetDir=$env:USERPROFILE\AppData\Local\Programs\Python\$Version",`
 	"Include_launcher=0",`
